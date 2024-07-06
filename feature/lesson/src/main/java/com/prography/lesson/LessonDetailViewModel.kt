aca273fc4b615b8e524fb595cb67cffb43de191f
ChoMk <gmk5432@naver.com>
2024-07-07 01:39:25 +0900

    #30 viewmodel 구성

--- feature/lesson/src/main/java/com/prography/lesson/LessonDetailViewModel.kt
+++ feature/lesson/src/main/java/com/prography/lesson/LessonDetailViewModel.kt
@@ -1,7 +1,46 @@
 package com.prography.lesson
 
+import NavigationEvent
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.prography.domain.lesson.usecase.LoadLessonDatesUseCase
+import com.prography.domain.lesson.usecase.LoadLessonUseCase
+import kotlinx.coroutines.flow.MutableSharedFlow
+
 /**
  * Created by MyeongKi.
  */
-class LessonDetailViewModel {
+class LessonDetailViewModel(
+    lessonId: Long,
+    navigateFlow: MutableSharedFlow<NavigationEvent>,
+    loadLessonUseCase: LoadLessonUseCase,
+    loadLessonDatesUseCase: LoadLessonDatesUseCase
+) : ViewModel() {
+    val machine = LessonDetailUiMachine(
+        lessonId = lessonId,
+        coroutineScope = viewModelScope,
+        navigateFlow = navigateFlow,
+        loadLessonUseCase = loadLessonUseCase,
+        loadLessonDatesUseCase = loadLessonDatesUseCase,
+    )
+
+    companion object {
+        fun provideFactory(
+            lessonId: Long,
+            navigateFlow: MutableSharedFlow<NavigationEvent>,
+            loadLessonUseCase: LoadLessonUseCase,
+            loadLessonDatesUseCase: LoadLessonDatesUseCase
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return LessonDetailViewModel(
+                    lessonId,
+                    navigateFlow,
+                    loadLessonUseCase,
+                    loadLessonDatesUseCase
+                ) as T
+            }
+        }
+    }
 }
