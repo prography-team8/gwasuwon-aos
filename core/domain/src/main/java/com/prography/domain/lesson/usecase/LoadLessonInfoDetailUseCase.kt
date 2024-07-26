a327bbbb871f47734dde01ef6be6ea6506bd28b4
ChoMk <gmk5432@naver.com>
2024-07-26 11:39:21 +0900

    N/A 사라진 코드 복원

---
+++ core/domain/src/main/java/com/prography/domain/lesson/usecase/LoadLessonInfoDetailUseCase.kt
@@ -0,0 +1,12 @@
+package com.prography.domain.lesson.usecase
+
+import com.prography.domain.lesson.model.Lesson
+import com.prography.domain.lesson.respository.LessonRepository
+import kotlinx.coroutines.flow.Flow
+
+
+class LoadLessonInfoDetailUseCase (private val repository: LessonRepository) {
+    operator fun invoke(lessonId: Long): Flow<Lesson> {
+        return repository.loadLessonInfoDetail(lessonId)
+    }
+}
